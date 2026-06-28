9c409dfc01c6fe75642f17e040b471fec7b9d5b4
sanithu <sanithu0902gamil .com>
2026-06-28 16:49:14 +0530

    fix port variable

--- app.py
+++ app.py
@@ -1462,5 +1462,5 @@ def logout():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
